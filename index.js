8492180c8864bea257c4ba70d18f619522ebd506
Jillu Rahman Jibon <jillurahmanjibon@gmail.com>
2022-11-09 14:27:58 +0600

    review data are get now

--- index.js
+++ index.js
@@ -27,13 +27,24 @@ async function run() {
 	const recipesCollection = client
 		.db("ReviewRecipesCollection")
 		.collection("recipes");
-	// post data
+	const reviewCollection = client
+		.db("ReviewRecipesCollection")
+		.collection("review");
+
+	// recipes post
+
 	app.post("/recipes", async (req, res) => {
 		const recipe = req.body;
 		const result = await recipesCollection.insertOne(recipe);
 		res.send(result);
 	});
-	// get data
+	// review post
+	app.post("/review", async (req, res) => {
+		const review = req.body;
+		const result = await reviewCollection.insertOne(review);
+		res.send(result);
+	});
+	// recipes get data
 	app.get("/limitRecipes", async (req, res) => {
 		const cursor = recipesCollection.find({});
 		const limitRecipes = await cursor.limit(3).toArray();
@@ -49,6 +60,19 @@ async function run() {
 		const query = { _id: ObjectId(id) };
 		const recipes = await recipesCollection.findOne(query);
 		res.send(recipes);
+	});
+
+	// review get data
+	app.get("/review", async (req, res) => {
+		const cursor = reviewCollection.find({});
+		const review = await cursor.toArray();
+		res.send(review);
+	});
+	app.get("/singleReviewId/:id", async (req, res) => {
+		const { id } = req.body;
+		const cursor = reviewCollection.find(id);
+		const result = await cursor.toArray();
+		res.send(result);
 	});
 }
 run().catch(err => {
